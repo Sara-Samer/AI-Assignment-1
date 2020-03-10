3c2237458b97043c3a82ca8de4a1b37092d7b402
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-03-10 22:10:19 +0200

    ..

--- Resource-Allocation Graph.pl
+++ Resource-Allocation Graph.pl
@@ -1,8 +1,9 @@
-%------------------- Sample test Case -----------------
+------------------- Sample test Case -----------------
 process(p1).
 process(p2).
 process(p3).
 process(p4).
+
 
 resource(r1).
 resource(r2).
@@ -20,19 +21,15 @@ requested(p2, r2).
 requested(p4, r4).
 
 available_instances([[r1, 5], [r2, 3], [r3, 0]]).
+available_instances(Available).
+process(P).
+can_run(P, Available).
 %------------------------------------
 
 % Rules Needed
 
-% check_availabe(---) -> takes a list of resources & list in
-% available_instances() and checks that all resources in list are availabe
+% check_Available(---) -> takes a list of resources & list in
+% available_instances() and checks that all resources in list are Available
 
 % release(---) -> takes a list of resources & list in
 % available_instances() and returns a new list after the resources are updated
-
-
-can_run(X):- get_list(X, L, LL).
-
-% get_list(---) -> takes a process and returns a list of it's requested resources
-get_list(X, L):- 
-    findall(Y, (requested(Z, Y), Z = X), L).
