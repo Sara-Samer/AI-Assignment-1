854534c177ee20f492b9066f93480d6284b6a01f
Sara Samer <36432332+9191DramaQueen@users.noreply.github.com>
2020-03-29 20:10:31 +0200

    submitted file

--- Resource-Allocation Graph.pl
+++ Resource-Allocation Graph.pl
@@ -7,29 +7,78 @@ process(p4).
 
 resource(r1).
 resource(r2).
-resource(r3).
-resource(r4).
 
 allocated(p1, r2).
 allocated(p2, r1).
 allocated(p3, r1).
 allocated(p4, r3).
 
-requested(p4, r1).
-requested(p2, r3).
+requested(p1, r1).
 requested(p2, r2).
-requested(p4, r4).
 
-available_instances([[r1, 5], [r2, 3], [r3, 0]]).
+available_instances([[r1, 0], [r2, 0]]).
 
 :- dynamic available_instances/1.
+:- dynamic original_instances/1.
 :- dynamic finished/1.
+:- dynamic safe_sequence/1.
+
+set():- 
+	available_instances(L), 
+	assert(original_instances(L)).
+
+reset():- 
+	original_instances(L),
+	update_available_instances(L).
 
 % check_availabe(---) -> takes a list of resources & list in
 % available_instances() and checks that all resources in list are availabe
 
-check_availabe([], _):-!.
+safe_state(X):-
+	set(),
+	run(),
+	get_finished_list(X),
+	clear_finished().
+	
+run():-
+	process(P),
+	(
+		not(finished(P)),
+		can_run(P), !
+	);
+	(
+		check_if_done(),
+		reset(),
+		set()
+	).
 
+can_run(P):-
+	get_request_list(P, Requested),
+	available_instances(Available),
+	check_availabe(Requested, Available),
+	get_allocated_list(P, Allocated), 
+	release(Allocated, Available, NewAvailable),
+	update_available_instances(NewAvailable),
+	add_finished(P),
+	run().
+
+
+
+release(L,[],[] ).
+
+release(L,[[R,I]|T],E ):-
+    member(R,L),!,
+    findall(R, member(R,L), Bag), 
+    length(Bag, NumOfInstances ),
+    I2 is I + NumOfInstances ,
+    E =[[R,I2]|T2],
+    release(L,T,T2).
+
+release(L,[[R,I]|T],E):-
+	E =[[R,I]|T2],
+	release(L,T,T2).
+
+check_availabe([], _):-!.
 check_availabe([H|T], A):-
 	check_all(H, A, New_list),
 	check_availabe(T, New_list).
@@ -38,86 +87,53 @@ check_all(H, [[R, N]|T], New_list):-
 	H = R,
 	N > 0,
 	N1 is N - 1,
-	New_list = [[R,N1]]|T],
+	append([[R, N1]], T, New_list),
 	!.
 
 check_all(H, [[R,N]|T], New_list):-
 	New_list = [[R, N]|T2],
 	check_all(H, T, T2).
 	
-safe_state(X):-
-	run(),
-	findall(P, finished(P), X).
-
-run():-
-	process(P),
-	available_instances(Available),
-	not(finished(P)),
-	can_run(P, Available).
-
-can_run(P, Available):- 
-	not(requested(P, _)), 
-	get_allocated_list(P, Allocated), 
-	release(Allocated, Available, NewAvailable),
-	update_available_instances(NewAvailable),
-	add_finished(P),
-	run().
-
-can_run(P, Available, NewAvailable):-
-	requested(P, _),
-	get_request_list(P, Requested),
-	check_availabe(Requested, Available),
-	get_allocated_list(P, Allocated), 
-	release(Allocated, Available, NewAvailable),
-	update_available_instances(NewAvailable),
-	add_finished(P),
-	run().
-
-add_finished(P):- assert(finished(P)).
-
 update_available_instances(NewAvailable):-
 	retract(available_instances(OldList)),
 	assert(available_instances(NewAvailable)).
-	
+
+clear_finished():-
+	get_finished_list(Finished),
+	clear_finished(Finished).
+clear_finished([]):-!.
+clear_finished([H|T]):-
+	retract(finished(H)),
+	clear_finished(T).
+
+% check_if_done():-
+% 	get_process_list(PL),
+% 	get_finished_list(FL),
+% 	sort(PL, P),
+%   sort(FL, F),
+% 	F == P.
+
+add_finished(P):- assert(finished(P)).
+add_sequence(X):- assert(safe_sequence(X)).
+
+check_if_done():-
+	get_process_list(PL),
+	get_finished_list(FL),
+	check_member(PL, FL).
+
+check_member([],_).
+check_member([H|T],Y):-
+    member(H,Y),
+    check_member(T,Y).
+
 get_request_list(P, L):- 
     findall(Y, (requested(Z, Y), Z = P), L).
 
 get_allocated_list(P, L):- 
     findall(Y, (allocated(Z, Y), Z = P), L).
-% release(---) -> takes a list of resources & list in
-% available_instances() and returns a new list after the resources are updated
 
-release(L,[],[] ).
+get_process_list(L):- 
+    findall(P, process(P), L).
 
-release(L,[[R|I]|T],E ):-
-    member(R,L),!,
-    findall(R, member(R,L), Bag), 
-    length(Bag, NumOfInstances ),
-    I2 is I + NumOfInstances ,
-    E =[[R|I2]|T2],
-    release(L,T,T2).
-
-release(L,[[R|I]|T],E):-
-	E =[[R|I]|T2],
-	release(L,T,T2).
-
-% release(_, _, _).
-% check_Available().
-% available_instances(Available).
-% process(P).
-% can_run(P, Available).
-
-% can_run(P, Available):- 
-%     not(requested(P, _)),
-%     %get list of process P allocated resources
-%     get_allocated_list(P, Allocated),
-%     %release all allocated resources
-%     release(Allocated, Available, NewAvailable).
-
-%can_run(P, Available):- requested(P, _), get_list(P, L, LL).
-
-% get_list(---) -> takes a process and returns a list of it's requested resources
-% get_request_list(P, L):- 
-%     findall(Y, (requested(Z, Y), Z = P), L).
-% get_allocated_list(P, L):- 
-%     findall(Y, (allocated(Z, Y), Z = P), L).
+get_finished_list(L):- 
+    findall(P, finished(P), L).
